f2a9f5c68469a6258262ab5056c03cb7e56798c4
Michael Onyeri <omichael003@gmail.com>
2024-05-01 21:55:46 -0500

    Intial commit

---
+++ Subscribe/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackExchange.Redis;
+
+using (ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379"))
+{
+    ISubscriber sub = redis.GetSubscriber();
+
+    //Subscribe to the channel named messages
+
+    sub.Subscribe("Craig", (channel, message) => {
+
+        //Output received message
+        Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] {message}");
+    });
+    Console.WriteLine("subscribed messages");
+    Console.ReadKey();
+}
---
+++ redis test/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+            new ConfigurationOptions
+            {
+                EndPoints = { "localhost:6379" }
+            });
+
+var db = redis.GetDatabase();
+var pong = await db.PingAsync();
+Console.WriteLine(pong);
+
+while (Console.ReadLine() != "Stop")
+{
+
+    Console.WriteLine("Enter a Key: ");
+    string? key = Console.ReadLine();
+    Console.WriteLine("Enter a value: ");
+    string? value = Console.ReadLine();
+
+
+    db.Publish(key, value);
+}
+
+
+//db.StringSet(key, value);
+
+
+//while (true)
+//{
+//    string searchValue = Console.ReadLine();
+
+
+//    if(db.StringGet(searchValue).ToString() == null)
+//    {
+//        Console.WriteLine("No Record Found");
+//    }
+//    else
+//    {
+//        Console.WriteLine("You found me!!! >> " + db.StringGet(searchValue).ToString());
+//        Console.ReadLine();
+//        break;
+//    }
+//}
+
+
