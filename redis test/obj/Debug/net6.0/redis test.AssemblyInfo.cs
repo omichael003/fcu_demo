1375864d577be5f1a5beca54c0046eb6f7cdd71e
Michael Onyeri <omichael003@gmail.com>
2024-05-13 11:04:56 -0500

    java version

--- redis test/obj/Debug/net6.0/redis test.AssemblyInfo.cs
+++ redis test/obj/Debug/net6.0/redis test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("redis test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a18199edcd6e584a86ffb85c72083c546d03a1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26054c0f13b1fe8525958b903ccfe1cfab01ddfd")]
 [assembly: System.Reflection.AssemblyProductAttribute("redis test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("redis test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
